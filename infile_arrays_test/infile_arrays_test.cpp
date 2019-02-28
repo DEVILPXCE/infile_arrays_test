d6e7eaa0786f979abe4c2474c1cb9bb5c802ec1d
CuiXinping <devilpxce@gmail.com>
2019-02-28 17:21:02 +0800

    17:20

--- infile_arrays_test/infile_arrays_test.cpp
+++ infile_arrays_test/infile_arrays_test.cpp
@@ -1,21 +1,88 @@
-﻿// infile_arrays_test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include"pch.h"
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
 
-#include "pch.h"
-#include <iostream>
+using namespace std;
+
+class Users
+{
+private:
+	string name;
+	string idd;
+	string password;
+public:
+	//Users();
+	//~Users();
+	void setUser(string nm, string id, string psw);
+	void display();
+	//void registe();
+};
+
+string randomID()
+{
+	string r;
+	srand(int(time(0)));
+	r = rand() % 100 + 100;
+	return r;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
-}
+	string nm;
+	string id;
+	string psw;
+	int n = 0;
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+	ofstream outfile("infile_arrays_test", ios::app);
 
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+	Users *p_user = new Users[n];
+
+	cout << "Please Complete Information:" << endl;
+	cout << "Enter Your Name:\nEnd With 'Enter'" << endl;
+	while (!getline(cin, nm))
+		cout << "NAME ERROR" << endl;
+	id = randomID();
+	cout << "This is Your ID:\n" << id << "\n" << "PLEASE TAKE DOWN" << endl;
+	cout << "Set Your PassWord:\n" << endl;
+	while (!getline(cin, psw))
+		cout << "PSW ERROR" << endl;
+	outfile << nm << id << psw;
+
+
+	outfile.close();
+
+	ifstream infile("infile_arrays_test", ios::in);
+
+	if (!infile)
+		cout << "OPEN ERROR" << endl;
+
+	while (getline(infile, nm) && getline(infile, id) && getline(infile, psw))
+		p_user[n++].setUser(nm, id, psw);
+
+	for (int i = 0; i <= n; i++)
+		p_user[i].display();
+
+	cout << "READ SUCCESS" << endl;
+
+	delete[]p_user;
+
+	infile.close();
+
+	return 0;
+
+};
+
+void Users::setUser(string nm, string id, string psw)
+{
+	name = nm;
+	idd = id;
+	password = psw;
+
+};
+void Users::display()
+{
+	cout << name << idd << password << endl;
+};
