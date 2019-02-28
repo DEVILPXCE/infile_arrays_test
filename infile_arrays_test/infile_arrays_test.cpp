665514232945d8d34ab18d7c793e5b09d0fe9631
CuiXinping <devilpxce@gmail.com>
2019-02-28 18:15:22 +0800

    18:15

--- infile_arrays_test/infile_arrays_test.cpp
+++ infile_arrays_test/infile_arrays_test.cpp
@@ -18,13 +18,13 @@ public:
 	void display();
 };
 
-string randomID()
-{
-	string r;
-	srand(int(time(0)));
-	r = rand() % 100 + 100;
-	return r;
-};
+//string randomID()
+//{
+//	string r;
+//	srand(int(time(0)));
+//	r = rand() % 100 + 100;
+//	return r;
+//};
 
 int main()
 {
@@ -35,34 +35,45 @@ int main()
 
 	ofstream outfile("infile_arrays_test", ios::app);
 
+	while (!outfile)
+		cout << "ERROR" << endl;
+
 	Users *p_user = new Users[n];
 
 	cout << "Please Complete Information:" << endl;
 	cout << "Enter Your Name:\nEnd With 'Enter'" << endl;
 	while (!getline(cin, nm))
 		cout << "NAME ERROR" << endl;
-	id = randomID();
-	cout << "This is Your ID:\n" << id << "\n" << "PLEASE TAKE DOWN" << endl;
+	//id = randomID();
+	//cout << "This is Your ID:\n" << id << "\n" << "PLEASE TAKE DOWN" << endl;
+	cout << "Set Your ID:\n" << endl;
+	while (!getline(cin, id))
+		cout << "ID ERROR" << endl;
 	cout << "Set Your PassWord:\n" << endl;
 	while (!getline(cin, psw))
 		cout << "PSW ERROR" << endl;
-	outfile << nm << id << psw;
+	outfile << nm << " ";
+	outfile << id << " ";
+	outfile << psw << "\n";
 
 
 	outfile.close();
 
 	ifstream infile("infile_arrays_test", ios::in);
 
-	if (!infile)
-		cout << "OPEN ERROR" << endl;
+	if (infile)
+		cout << "OPEN SUCCEED" << endl;
 
-	while (getline(infile, nm) && getline(infile, id) && getline(infile, psw))
+	//while (getline(infile, nm) && getline(infile, id) && getline(infile, psw))
+	while(infile>>nm>>id>>psw)
+	{
 		p_user[n++].setUser(nm, id, psw);
-
+		cout << "READ SUCCEED" << endl;
+	}
 	for (int i = 0; i <= n; i++)
 		p_user[i].display();
 
-	cout << "READ SUCCESS" << endl;
+	cout << "DISPLAY SUCCEED" << endl;
 
 	delete[]p_user;
 
@@ -70,6 +81,10 @@ int main()
 
 	return 0;
 
+};
+void Users::display()
+{
+	cout << name<<" "<< userid <<" "<< password <<" "<< endl;
 };
 
 void Users::setUser(string nm, string id, string psw)
@@ -79,7 +94,4 @@ void Users::setUser(string nm, string id, string psw)
 	password = psw;
 
 };
-void Users::display()
-{
-	cout << name << userid << password << endl;
-};
+
